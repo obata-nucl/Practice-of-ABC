4013fb5b9a0ed5b356898b1f5bbe99bf5aad4e64
obata-nucl <obata0904@outlook.jp>
2024-04-27 23:41:08 +0900

    submit 351-A

--- 351/A.cpp
+++ 351/A.cpp
@@ -3,5 +3,18 @@ using namespace std;
 #define rep(i,n) for (int i=0;i<(n);++i)
 
 int main(){
-    int n;
+    int sum_a =0,sum_b=0;
+    rep(i,9){
+        int n;
+        cin >> n;
+        sum_a += n;
+    }
+    rep(i,8){
+        int m;
+        cin >> m;
+        sum_b += m;
+    }
+
+    cout << sum_a - sum_b +1 << endl;
+    return 0;
 }
