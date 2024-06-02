28087e0ce712eff95ca9c1faa370e8276cf61bd9
obata-nucl <obata0904@outlook.jp>
2024-06-02 14:05:54 +0900

    submit 356-A

---
+++ 356/A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0;i<(n);++i)
+
+int main(){
+    int n,l,r;
+    cin >> n >> l >> r;
+
+    rep(i,l-1){
+        cout << i+1 << " ";
+    }
+    for(int i=r;i>l-1;--i){
+        cout << i << " ";
+    }
+    for(int i=r;i<n;++i){
+        cout << i+1 << " ";
+    }
+    cout << endl;
+    return 0;
+}
